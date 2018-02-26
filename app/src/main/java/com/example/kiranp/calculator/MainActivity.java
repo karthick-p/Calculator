77b768028555099c3799ed0a4e5aa8265ef96776
Karthick Parameswaran <kr688977@dal.ca>
2018-02-25 22:53:54 -0400

    Final Commit
    Application developed with polished UI

--- app/src/main/java/com/example/kiranp/calculator/MainActivity.java
+++ app/src/main/java/com/example/kiranp/calculator/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.kiranp.calculator;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     String new_num = "0";
     String con_num = "0";
     String operand_sym = "";
@@ -23,20 +18,34 @@ public class MainActivity extends AppCompatActivity{
     Queue trial = new LinkedList();
     Queue b = new LinkedList();
     Queue c = new LinkedList();
-    String temp ="";
+    String temp = "";
     String temp_calc = "0";
     String root_flag = "0";
+    String all_flag = "0";
+    String toggle_flag = "0";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         output = findViewById(R.id.result);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);}
+        setContentView(R.layout.activity_main);
+    }
 
-    public void buttonpress(View button) {
+    public void buttonpress(View button)/*On Click listener for buttons*/ {
         output = findViewById(R.id.result);
-        switch (button.getId()){
+        (findViewById(R.id.n0)).setEnabled(true);
+        (findViewById(R.id.add)).setEnabled(true);
+        (findViewById(R.id.sub)).setEnabled(true);
+        (findViewById(R.id.mul)).setEnabled(true);
+        (findViewById(R.id.div)).setEnabled(true);
+        (findViewById(R.id.sqrt)).setEnabled(true);
+        toggle_flag = "0";
+        all_flag = "0";
+
+
+        switch (button.getId()) {
             case R.id.n0:
                 new_num = "0";
                 output.append("0");
@@ -78,36 +87,58 @@ public class MainActivity extends AppCompatActivity{
                 output.append("9");
                 break;
             case R.id.dot:
-                new_num =".";
+                new_num = ".";
                 output.append(".");
                 break;
         }
-       // System.out.println(new_num);
-        if (!(root_flag.equals("1"))){
+        // System.out.println(new_num);
+        if (!(root_flag.equals("1"))) {
             trial.add(new_num);
-            compute();}
-        else{
-            //c.add(new_num);
-            //root();
-            }
+            compute();
+        }
+
     }
-    public void operand(View oper){
-        String x ="";
+
+    public void operand(View oper)/*On Click listener for operands*/ {
+        (findViewById(R.id.n0)).setEnabled(true);
+        String x = "";
         output = findViewById(R.id.result);
         TextView result2 = findViewById(R.id.result2);
-        if(root_flag.equals("1"))
+        if (root_flag.equals("1"))
             trial = b;
         root_flag = "0";
-        switch (oper.getId()){
+        all_flag = "0";
+        toggle_flag = "0";
+        switch (oper.getId()) {
             case R.id.add:
                 operand_sym = "+";
                 output.append("+");
+                (findViewById(R.id.sub)).setEnabled(false);
+                (findViewById(R.id.div)).setEnabled(false);
+                (findViewById(R.id.mul)).setEnabled(false);
+                (findViewById(R.id.sqrt)).setEnabled(false);
+                (findViewById(R.id.add)).setEnabled(false);
+                (findViewById(R.id.toggle)).setEnabled(false);
                 break;
             case R.id.sub:
                 operand_sym = "-";
                 output.append("-");
+                (findViewById(R.id.add)).setEnabled(false);
+                (findViewById(R.id.div)).setEnabled(false);
+                (findViewById(R.id.mul)).setEnabled(false);
+                (findViewById(R.id.sqrt)).setEnabled(false);
+                (findViewById(R.id.sub)).setEnabled(false);
+                (findViewById(R.id.toggle)).setEnabled(false);
+
+
                 break;
             case R.id.mul:
+                (findViewById(R.id.sub)).setEnabled(false);
+                (findViewById(R.id.div)).setEnabled(false);
+                (findViewById(R.id.add)).setEnabled(false);
+                (findViewById(R.id.sqrt)).setEnabled(false);
+                (findViewById(R.id.mul)).setEnabled(false);
+                (findViewById(R.id.toggle)).setEnabled(false);
                 operand_sym = "*";
                 x = "\u00D7";
                 output.append(x);
@@ -116,6 +147,13 @@ public class MainActivity extends AppCompatActivity{
                 operand_sym = "/";
                 x = "\u00F7";
                 output.append(x);
+                (findViewById(R.id.n0)).setEnabled(false);
+                (findViewById(R.id.sub)).setEnabled(false);
+                (findViewById(R.id.div)).setEnabled(false);
+                (findViewById(R.id.mul)).setEnabled(false);
+                (findViewById(R.id.sqrt)).setEnabled(false);
+                (findViewById(R.id.add)).setEnabled(false);
+                (findViewById(R.id.toggle)).setEnabled(false);
                 break;
             case R.id.sqrt:
                 operand_sym = "rt";
@@ -123,58 +161,94 @@ public class MainActivity extends AppCompatActivity{
                 x = "\u221A";
                 output.append(x);
                 break;
-            case R.id.all:
-                trial.removeAll(trial);
+            case R.id.ac:
+                all_flag = "1";
+                for (int i = 0; i < trial.size(); i++)
+                    trial.poll();
+                for (int i = 0; i < b.size(); i++)
+                    b.poll();
                 result2.setText("0");
+                output.setText("");
+                break;
+            case R.id.toggle:
+                toggle_flag = "1";
+                String var = (String) result2.getText();
+                Double a = Double.parseDouble(var);
+                a = a * (-1);
+                var = Double.toString(a);
+                result2.setText(var);
+                break;
         }
-       // System.out.println(operand_sym);
-        if (!(root_flag.equals("1"))){
-            trial.add(operand_sym);
-            compute();}
-        else {
-          //  c.add(operand_sym);
-           // root();
+        // System.out.println(operand_sym);
+        if (!((all_flag.equals("1")) || (toggle_flag.equals("1")))) {
+            if (!((root_flag.equals("1")))) {
+                trial.add(operand_sym);
+                compute();
+            }
         }
     }
-    public void compute(){
-        TextView result2 = findViewById(R.id.result2);
-        Iterator<Integer> it = trial.iterator();
-        while(it.hasNext())  {
-            b.add(it.next());
-        }
-        String number_patch = "";
-        String temp;
-        String operand = "";
-        Double total = 1.0;
-        Double second_number = 1.0;
-        int i =0;
-        Double first = 0.0;
-        String first_patch = "";
-        String flag = "0";
-        while (!(b.isEmpty())){
-            temp = (String) b.remove();
-            if(!(temp.equals("+") || temp.equals("-") || temp.equals("*") || temp.equals("/"))){
-                first_patch += temp;
-                first = Double.parseDouble(first_patch);
-            }
-            else{
-                operand = temp;
-                //System.out.println("break");
-                number_patch = "";
-                break;}
-        }
-        total = first;
-       // System.out.println(total);
 
-        while (!(b.isEmpty())){
-            temp = (String) b.remove();
-            if(!(temp.equals("+") || temp.equals("-") || temp.equals("*") || temp.equals("/"))){
-                number_patch += temp;
-                second_number = Double.parseDouble(number_patch);
-                flag = "1";
+    public void compute()/*runtime computing */ {
+        TextView result2 = findViewById(R.id.result2);
+        if (all_flag.equals("0")) {
+            Iterator<Integer> it = trial.iterator();
+            while (it.hasNext()) {
+                b.add(it.next());
+            }
+            String number_patch = "";
+            String temp;
+            String operand = "";
+            Double total = 1.0;
+            Double second_number = 1.0;
+            int i = 0;
+            Double first = 0.0;
+            String first_patch = "";
+            String flag = "0";
+            while (!(b.isEmpty())) {
+                temp = (String) b.remove();
+                if (!(temp.equals("+") || temp.equals("-") || temp.equals("*") || temp.equals("/"))) {
+                    first_patch += temp;
+                    first = Double.parseDouble(first_patch);
+                } else {
+                    operand = temp;
+                    //System.out.println("break");
+                    number_patch = "";
+                    break;
                 }
-            else {
-                if (flag.equals("1")){
+            }
+            total = first;
+            // System.out.println(total);
+
+            while (!(b.isEmpty())) {
+                temp = (String) b.remove();
+                if (!(temp.equals("+") || temp.equals("-") || temp.equals("*") || temp.equals("/"))) {
+                    number_patch += temp;
+                    second_number = Double.parseDouble(number_patch);
+                    flag = "1";
+                } else {
+                    if (flag.equals("1")) {
+                        switch (operand) {
+                            case "+":
+                                total = total + second_number;
+                                break;
+                            case "-":
+                                total = total - second_number;
+                                break;
+                            case "*":
+                                total = total * second_number;
+                                break;
+                            case "/":
+                                total = total / second_number;
+                                break;
+                        }
+                        flag = "0";
+                    }
+                    operand = temp;
+                    //  System.out.println(operand);
+                    number_patch = "";
+                }
+            }
+            if (flag.equals("1")) {
                 switch (operand) {
                     case "+":
                         total = total + second_number;
@@ -189,61 +263,44 @@ public class MainActivity extends AppCompatActivity{
                         total = total / second_number;
                         break;
                 }
-                flag = "0";}
-                operand = temp;
-              //  System.out.println(operand);
-                number_patch = "";
+                flag = "0";
             }
-        }
-        if(flag.equals("1")){
-            switch (operand) {
-                case "+":
-                    total = total + second_number;
-                  //  System.out.println(total);
-                    break;
-                case "-":
-                    total = total - second_number;
-                    break;
-                case "*":
-                    total = total * second_number;
-                    break;
-                case "/":
-                    total = total / second_number;
-                    break;
-            }
-            flag = "0";}
-            System.out.println(total);
-            String out ="";
+            String out = "";
             int o;
-            if (Math.round(total)==total){
-                o = (int) Math.round(total);;
-                out = String.valueOf(o);}
-            else
+            if (Math.round(total) == total) {
+                o = (int) Math.round(total);
+                ;
+                out = String.valueOf(o);
+            } else
                 out = Double.toString(total);
-        System.out.println(out);
-        result2.setText(out);
+            result2.setText(out);
+        }
     }
-    public void delete(View del){
+
+    public void delete(View del)/*For single clear entry*/ {
         String text = output.getText().toString();
         output.setText(text.substring(0, text.length() - 1));
         Queue queue2 = new LinkedList();
-/*https://stackoverflow.com/questions/4808852/remove-the-last-element-in-a-queue - for removing last element in queue*/
-        while(!trial.isEmpty()){
+/* Referred - https://stackoverflow.com/questions/4808852/remove-the-last-element-in-a-queue - for removing last element in queue*/
+        while (!trial.isEmpty()) {
             Object o = trial.remove();
-            if(!trial.isEmpty()){
+            if (!trial.isEmpty()) {
                 queue2.add(o);
             }
         }
-         trial = queue2;
-         compute();
+        if (!trial.isEmpty()) {
+            trial = queue2;
+            compute();
+        }
     }
-    public void root() {
+
+    public void root()/*Square root function*/ {
         Queue d = new LinkedList();
         Iterator<Integer> it = c.iterator();
         while (it.hasNext()) {
             d.add(it.next());
         }
-        String temp ="";
+        String temp = "";
         String rt_concat = "";
         Double value = 0.0;
         Double a = 0.0;
@@ -253,20 +310,19 @@ public class MainActivity extends AppCompatActivity{
             if (!(temp.equals("rt"))) {
                 rt_concat += temp;
                 value = Double.parseDouble(rt_concat);
+            } else {
+                break;
             }
-            else{
-                break;}
 
         }
 
         if (!(value.equals(0.0))) {
-                value = Double.parseDouble(rt_concat);
-                a = Math.sqrt(value);
-                rt_concat = Double.toString(a);
-                System.out.println(a);
-               // b.add(rt_concat);
-            }
-          // System.out.println(c.size());
+            value = Double.parseDouble(rt_concat);
+            a = Math.sqrt(value);
+            rt_concat = Double.toString(a);
+            System.out.println(a);
+            // b.add(rt_concat);
+        }
     }
 }
 
